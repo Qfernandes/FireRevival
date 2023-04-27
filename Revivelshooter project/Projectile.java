ba14d2fa1edae005e9d30517b4725f771c2bd4f5
Sakthy1 <114680260+Sakthy1@users.noreply.github.com>
2023-04-27 14:22:08 +0100

    final

---
+++ Revivelshooter project/Projectile.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Projectile here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Projectile extends Actor
+{
+        
+    // Create Projectile Projectline Colour: RED
+    public Projectile()
+    {
+        setImage(new GreenfootImage(10,2));
+        getImage().setColor(Color.RED);
+        getImage().fillRect(0,0,10,2);
+    }
+    public void act()
+    {
+        //move the Projectile , used Greenfoot.PlaySound:"pistol-shot.mp3" form free music store with no copyrights.
+        move(10);
+        if(isAtEdge())
+        {
+            getWorld().removeObject(this);
+            //apply  the shooting sound 
+            Greenfoot.playSound("pistol-shot.mp3");
+        }
+    }
+    
+}
---
+++ Revivelshooter project/SuperPower.java
@@ -0,0 +1,51 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class SuperPower here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SuperPower extends Actor
+{
+    /**
+     * Act - do whatever the SuperPower wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    // SUPER POWER LIMIT is  constant and set to 100
+    //superCount starts from 0
+    final int SUPER_POWER_LIMIT =100;
+    int superCount;
+    int count;
+    public SuperPower()
+    {
+        setImage(new GreenfootImage(SUPER_POWER_LIMIT + 2, 12));
+        //set the image  and color of SUPER_POWER_LIMIT
+        getImage().drawRect(0,0,SUPER_POWER_LIMIT +1,11);
+        getImage().setColor(Color.GREEN);
+        getImage().fillRect(1,1,superCount,10);
+        
+    }
+    public void act()
+    {
+        setImage(new GreenfootImage(52, 12));
+        getImage().drawRect(0,0,51,11);
+        getImage().setColor(Color.GREEN);
+        getImage().fillRect(1,1,superCount,10);
+        World world = getWorld();
+        MyWorld myWorld = (MyWorld)world;
+        //set the location
+        setLocation(myWorld.getPlayer().getX()+10 , myWorld.getPlayer().getY()-80);
+        useSuper();
+    }
+    public  void useSuper()
+    {
+        count++;
+        if(count % 5==0)
+        {
+           superCount++;
+        }
+       
+    }
+}
+
